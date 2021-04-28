0fd28535578fcea99bf1bbb746436ae85316b88e
Joe Healy <josephehealy@hotmail.com>
2021-04-28 17:39:07 -0400

    initial checkin

---
+++ ConsoleHelperSLN/ConsoleHelper/devfish.ConsoleHelper.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace devfish.ConsoleHelper
+{
+    public static class CH
+    {
+        static public ConsoleColor AlertColor { get; set; } = ConsoleColor.Yellow;
+        static public ConsoleColor ErrColor { get; set; } = ConsoleColor.Red;
+        static public ConsoleColor MsgColor { get; set; } = ConsoleColor.Green;
+
+        public static void Info(string key, string val)
+        {
+            if (String.IsNullOrWhiteSpace(key) && String.IsNullOrWhiteSpace(val)) return;
+            key = String.IsNullOrWhiteSpace(key) ? "<empty>" : key;
+            val = String.IsNullOrWhiteSpace(val) ? "<empty>" : val;
+            Info($"[{key}] =[{val}]");
+        }
+
+        public static void Info(string msg)
+        {
+            ConsoleColor oldColor = Console.ForegroundColor;
+            if (String.IsNullOrWhiteSpace(msg)) return;
+            Console.ForegroundColor = CH.AlertColor;
+            Console.WriteLine(msg);
+            Console.ForegroundColor = oldColor;
+        }
+
+        public static void Err(string key, string val)
+        {
+            if (String.IsNullOrWhiteSpace(key) && String.IsNullOrWhiteSpace(val)) return;
+            key = String.IsNullOrWhiteSpace(key) ? "<empty>" : key;
+            val = String.IsNullOrWhiteSpace(val) ? "<empty>" : val;
+
+            Err($"[{key}] =[{val}]");
+        }
+
+        public static void Err(string msg)
+        {
+            ConsoleColor oldColor = Console.ForegroundColor;
+            if (String.IsNullOrWhiteSpace(msg)) return;
+            Console.ForegroundColor = CH.ErrColor;
+            Console.WriteLine(msg);
+            Console.ForegroundColor = oldColor;
+        }
+
+        public static void Msg(string key, string val)
+        {
+            if (String.IsNullOrWhiteSpace(key) && String.IsNullOrWhiteSpace(val)) return;
+            key = String.IsNullOrWhiteSpace(key) ? "<empty>" : key;
+            val = String.IsNullOrWhiteSpace(val) ? "<empty>" : val;
+
+            Msg($"[{key}] =[{val}]");
+        }
+
+        public static void Msg(string msg)
+        {
+            ConsoleColor oldColor = Console.ForegroundColor;
+            Console.ForegroundColor = CH.MsgColor;
+            if (String.IsNullOrWhiteSpace(msg)) return;
+            Console.WriteLine(msg);
+            Console.ForegroundColor = oldColor;
+        }
+
+        public static void NL()
+        {
+            Console.WriteLine();
+        }
+
+        public static string Pause()
+        {
+            return Pause("Hit any key to continue.... devfish.ConsoleHelper says bye....");
+        }
+
+        public static string Pause( string s )
+        {
+            CH.Info(s);
+            return Console.ReadLine();
+        }
+    }
+}
---
+++ ConsoleHelperSLN/ConsoleHelperDemo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using devfish.ConsoleHelper;
+
+namespace ConsoleHelperDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CH.Info("this is info");
+            CH.Msg("this is Msg");
+            CH.Err("this is err");
+
+            CH.Msg("changing colors");
+            ConsoleColor saveColor = CH.MsgColor;
+            CH.MsgColor = ConsoleColor.Cyan;
+            CH.Msg("cyan here");
+            CH.MsgColor = saveColor;
+            CH.Msg("color restored");
+            
+            CH.Pause("this is pause, custom message, hit any key to exit");
+        }
+    }
+}
